9f68552b172f123f2ddab7604d4044ac21cff919
MomchilAngelovv <momchilangelov0040@gmail.com>
2020-05-05 14:47:27 +0300

    confirmed leads and picture changes

---
+++ Web/GoldLeadsMedia.Web/wwwroot/js/affiliates/details.js
@@ -0,0 +1,13 @@
+﻿$('.date-range').daterangepicker({
+    ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    "alwaysShowCalendars": true,
+}, function (start, end, label) {
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+});
