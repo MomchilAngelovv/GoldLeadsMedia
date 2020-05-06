30e43175f654c130ae67e30aba4ae7f632d802a6
MomchilAngelovv <momchilangelov0040@gmail.com>
2020-05-06 12:54:11 +0300

    Add service for ftd scan

--- CoreApi/GoldLeadsMedia.CoreApi/Controllers/ClicksController.cs
+++ CoreApi/GoldLeadsMedia.CoreApi/Controllers/ClicksController.cs
@@ -8,15 +8,19 @@
     using GoldLeadsMedia.CoreApi.Models.InputModels;
     using GoldLeadsMedia.CoreApi.Services.Application.Common;
     using GoldLeadsMedia.CoreApi.Models.ServiceModels;
+    using GoldLeadsMedia.Database;
+    using Microsoft.AspNetCore.Builder;
 
     public class ClicksController : ApiController
     {
         private readonly IClicksService clicksService;
+        private readonly GoldLeadsMediaDbContext db;
 
         public ClicksController(
-            IClicksService clicksService)
+            IClicksService clicksService, GoldLeadsMediaDbContext db)
         {
             this.clicksService = clicksService;
+            this.db = db;
         }
 
         [HttpPost]
@@ -40,6 +44,20 @@
             };
 
             return response;
+        }
+
+        [HttpPost("test")]
+        public async Task<ActionResult<string>> Test()
+        {
+            var testError = new DeveloperError
+            {
+                Message = "From worker"
+            };
+
+            await this.db.DeveloperErrors.AddAsync(testError);
+            await this.db.SaveChangesAsync();
+
+            return testError.Id;
         }
     }
 }
---
+++ FtdScanWorker/FtdScanWorker.Service/FtdScan.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FtdScanWorker.Service
+{
+    public class FtdScan : IHostedService, IDisposable
+    {
+        private readonly Timer timer;
+
+        public FtdScan()
+        {
+            this.timer = new Timer(Scan, null, TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(10));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void Scan(object state)
+        {
+            var httpCleint = new HttpClient();
+
+            var response = httpCleint.PostAsync("https://localhost:44322/api/clicks/test", null).GetAwaiter().GetResult();
+            var responseAsString = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            Console.WriteLine(responseAsString);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.timer?.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            this.timer?.Dispose();
+        }
+    }
+}
